101f4f1e800e1a559a54deb3043068112ad3ab71
rohanbrit <rohanbrit@gmail.com>
2023-09-29 15:13:26 +1000

    Cookiecutter data science template added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Assignment 2 for Advanced Machine Learning Application',
+    author='Rohan Britto',
+    license='',
+)
